c331846dd3fbedf7c1cb351aad1e85b4a5ad6167
Carlos Salgado <carlos.d.salgado@gmail.com>
2019-01-07 12:34:50 -0200

    Configuracao de testes

--- src/test/java/com/cadu/pontoeletronico/api/MeuPrimeiroProjetoApplicationTests.java
+++ src/test/java/com/cadu/pontoeletronico/api/MeuPrimeiroProjetoApplicationTests.java
@@ -3,11 +3,13 @@ package com.cadu.pontoeletronico.api;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("test")
 public class MeuPrimeiroProjetoApplicationTests {
 
 	@Test
