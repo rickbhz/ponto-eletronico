51d8dc25633405b8501a81bfe687a45a81b35f27
Carlos Salgado <carlos.d.salgado@gmail.com>
2019-01-15 10:05:49 -0200

    teste

--- src/main/java/com/cadu/pontoeletronico/api/utils/WebSecurityConfig.java
+++ src/main/java/com/cadu/pontoeletronico/api/utils/WebSecurityConfig.java
@@ -6,7 +6,7 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@EnableWebSecurity
+@EnableWebSecurity 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
