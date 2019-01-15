b916b11d0463f557d8ec38b8223ade76bdaa95ff
Carlos Salgado <carlos.d.salgado@gmail.com>
2019-01-15 09:05:36 -0200

    Ajuste

--- src/test/java/com/cadu/pontoeletronico/api/controllers/LancamentoControllerTest.java
+++ src/test/java/com/cadu/pontoeletronico/api/controllers/LancamentoControllerTest.java
@@ -13,8 +13,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.BDDMockito;
 import org.mockito.Mockito;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
